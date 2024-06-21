620959c9fe954f433026e28b0c44d777579cc259
franklinniy <franklinniy@gmail.com>
2024-06-21 15:52:53 -0700

    task6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - is the entry point of the program.
+ *
+ * Description: if statements
+ *
+ * Return: the result is always 0 to show success
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar("%d", n);
+	}
+	printf('\n');
+	return (0);
+}
