392ba39b5324f88054b6ba0d5cc970f4681b09e4
franklinniy <franklinniy@gmail.com>
2024-06-21 15:08:04 -0700

    task4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry point of the program.
+ *
+ * Description: if statements
+ *
+ * Return: the result is always 0 to show success
+*/
+int main(void)
+{
+	char ch;
+	char CH;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (CH = 'A'; CH <= 'Z'; ch++)
+	{
+		putchar(CH);
+	}
+	putchar('\n');
+	return (0);
+}
