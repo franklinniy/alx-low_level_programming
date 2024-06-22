2a6e1fa4a2e7732dfd8990fc2571d9da83a3032f
franklinniy <franklinniy@gmail.com>
2024-06-21 17:16:44 -0700

    task11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - is the entry point of the program.
+ *
+ * Description: if statements
+ *
+ * Return: the result is always 0 to show success
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
