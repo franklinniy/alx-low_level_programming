cb257cd93c861f410f1a1549f2d2fb5555ef07a3
franklinniy <franklinniy@gmail.com>
2024-06-23 23:23:11 -0700

    advanced2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - is the entry point of the program.
+ *
+ * Description: if statements
+ *
+ * Return: the result is always 0 to show success
+*/
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	return (0);
+}
