7c78894a5143a8ae19318786b05e9b99d1d5928f
franklinniy <franklinniy@gmail.com>
2024-06-26 01:52:31 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is the entry point of the program.
+ *
+ * Description: if statements
+ *
+ * Return: the result is always 0 to show success
+*/
+int main(void)
+{
+	printf("_putchar\n");
+}
