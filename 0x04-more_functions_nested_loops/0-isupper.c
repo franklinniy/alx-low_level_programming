b411750143884aa1c86ca64ac5f5c535ba293922
franklinniy <franklinniy@gmail.com>
2024-06-27 14:20:21 +0300

    supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,38 @@
  *
  * Return: the result is always 0 to show success
 */
+int _isupper(int c);
+int main(void)
+{
+	char test_char1 = 'A';
+	char test_char2 = 'a';
+
+	if (_isupper(test_char1))
+	{
+		printf("%c: 1\n", test_char1);
+	}
+	else
+	{
+		printf("%c: 0\n", test_char1);
+	}
+	if (_isupper(test_char2))
+	{
+		printf("%c: 1\n", test_char2);
+	}
+	else
+	{
+		printf("%c: 0\n", test_char2);
+	}
+	return (0);
+}
+
 int _isupport(int c)
 {
-	if (c >= 'A' && c<= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
