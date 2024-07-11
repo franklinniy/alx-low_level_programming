0212e33c65c543a97ea5703de48b793581a0b3fb
franklinniy <franklinniy@gmail.com>
2024-07-11 03:11:38 +0300

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *_puts_recursion - prints a string then a new line.
+ *@s:The string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return ();
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "Hello, World!";
+
+	_puts_recursion(str);
+	return (0);
+}
+
