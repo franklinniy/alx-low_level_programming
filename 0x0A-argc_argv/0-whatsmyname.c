bbbe66a09474957fb933e7034cd3581b19bc7d2a
franklinniy <franklinniy@gmail.com>
2024-07-18 03:55:54 +0300

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
 		printf("%s\n", argv[0]);
 	}
-	return(0);
+	return (0);
 }
