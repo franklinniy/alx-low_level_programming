0be129b4945349af3f5ff3e0a0c34da5609d1d52
franklinniy <franklinniy@gmail.com>
2024-08-31 04:20:36 +0300

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,10 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *message;
+
+	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, message, sizeof(message) - 1);
 	return (1);
 }
