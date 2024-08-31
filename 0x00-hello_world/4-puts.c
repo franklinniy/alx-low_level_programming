581bbeda9b3dce459e370192bac018385531851d
franklinniy <franklinniy@gmail.com>
2024-08-31 04:01:14 +0300

    yay

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 if successful.
+ *
+ * This function prints a message to the console using 'puts' and returns 0.
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
