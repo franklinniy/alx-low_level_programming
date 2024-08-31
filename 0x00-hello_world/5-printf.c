a14eb5bbbc7ab9e6e4985707b66adcbef85e2d9f
franklinniy <franklinniy@gmail.com>
2024-08-31 04:05:02 +0300

    yaay

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
+ *
+ * This program prints a message using 'printf' with proper grammar
+ * and ends with a new line. It returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
